6b698cf251027bf8fe8887a7074182cb5630636c
yyj14 <yyj1453@naver.com>
2023-05-02 12:54:12 +0900

    05/02 use resources

--- app/src/main/java/org/techtown/kotlinpractice09/MainActivity.kt
+++ app/src/main/java/org/techtown/kotlinpractice09/MainActivity.kt
@@ -2,10 +2,16 @@ package org.techtown.kotlinpractice09
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import org.techtown.kotlinpractice09.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = getString(R.string.txt_data1)
+        binding.textView.setTextColor(ResourcesCompat.getColor(resources, R.color.txt_color, null))
+        binding.textView.textSize = resources.getDimension(R.dimen.txt_size)
     }
 }
